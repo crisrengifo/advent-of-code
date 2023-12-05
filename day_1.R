74d31cb24d64a3f3215d25259fe91aaf0208e206
Cristobal Rengifo Castillo <cristobal.rengifo-castillo@defra.gov.uk>
2023-12-05 11:59:44 +0000

    day 1 complete

--- day_1.R
+++ day_1.R
@@ -8,15 +8,30 @@ values_extracted <- str_extract_all(text, "[:digit:]")
 values_extracted <- map(values_extracted, \(x) paste(x[c(1, length(x))], collapse = ""))
 sum(as.integer(unlist(values_extracted)))
 
-# Part 2: Working
+# Part 2: Complete
 text <- readLines("inputs/day_1_2")
-regex_numbers <- paste(
-  c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "[:digit:]"), 
-  collapse = "|"
-)
-values_extracted <- str_extract_all(text, regex_numbers)
 
-values_extracted <- map(values_extracted, \(x) x[c(1, length(x))]) |> 
+regex_numbers <- c(
+  "one", "two", "three", "four", "five", 
+  "six", "seven", "eight", "nine", "[:digit:]"
+)
+
+# Get list of positions
+get_positions <- function(txt) {
+  str_locate_all(txt, regex_numbers) |> 
+    discard(is_empty) |> 
+    map(\(x) tibble(start = x[,1], end = x[,2])) |> 
+    list_rbind() |> 
+    arrange(start)
+}
+
+pos <- map(text, get_positions) 
+
+# Use positions to get numbers
+numbers_extracted <- map2(text, pos, \(x, y) str_sub(x, y[[1]], y[[2]]))
+
+# Translate to digits
+digits_extracted <- map(numbers_extracted, \(x) x[c(1, length(x))]) |> 
   map(
     \(x) case_when(
       x == "one"   ~ "1", 
@@ -32,5 +47,6 @@ values_extracted <- map(values_extracted, \(x) x[c(1, length(x))]) |>
     )
   )
 
-values_extracted <- map(values_extracted, \(x) paste(x, collapse = ""))
+# Get result
+values_extracted <- map(digits_extracted, \(x) paste(x, collapse = ""))
 sum(as.integer(unlist(values_extracted)))
